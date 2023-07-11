5ffb424898f26937a4678c7e8939e45716b5908e
Arnol Cáceres <116393375+arnolcb@users.noreply.github.com>
2023-07-10 20:55:16 -0500

    feat: web files

---
+++ app.js
@@ -0,0 +1,33 @@
+let pokeNameInput = document.getElementById("search");
+const searchBtn = document.getElementById("search-button");
+const pokeImage = document.getElementById("poke-image");
+const pokeNName = document.getElementById("poke-name");
+const pokeId = document.getElementById("poke-id");
+const pokeType = document.getElementById("poke-type");
+const pokeAbility = document.getElementById("poke-ability");
+
+searchBtn.addEventListener("click", async () => {
+  let pokeName = pokeNameInput.value.toLowerCase();
+
+  let llamadaApi = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`);
+  let respuesta = await llamadaApi.json();
+
+  //Test
+  console.log(respuesta.forms[0]);
+
+  // Obtener la URL de la imagen
+  let imageUrl = respuesta.sprites.front_default;
+
+  let name = respuesta.forms[0].name;
+  let id = respuesta.id;
+  let type = respuesta.types[0].type.name;
+  let ability = respuesta.abilities[0].ability.name;
+
+  // Asignar la URL de la imagen al atributo src del elemento de imagen
+  pokeImage.src = imageUrl;
+
+  pokeNName.innerHTML = "Nombre: " + name;
+  pokeId.innerHTML = "ID: " + id;
+  pokeType.innerHTML = "Tipo: " + type;
+  pokeAbility.innerHTML = "Habilidad: " + ability;
+});
